1871a82579f17786862ed73f8e8d4e1d983212c2
Cooper Thompson <34377046+cooperthompson55@users.noreply.github.com>
2025-05-11 03:36:33 -0400

    Deploy application to Netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PackageBuilder from './components/PackageBuilder';
 
 function App() {
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(() => {
+      window.parent.postMessage(
+        {
+          type: 'resize',
+          height: document.documentElement.scrollHeight,
+        },
+        '*'
+      );
+    });
+
+    resizeObserver.observe(document.body);
+
+    return () => resizeObserver.disconnect();
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
       <PackageBuilder />
